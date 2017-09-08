e2154cb23744ac1479a40d22c63a9ee36f8183b6
dongfangqiaoen <sunyangdaxue@sina.com>
2017-09-08 10:30:51 +0800

    格式化代码，测试提交

--- sample/src/main/java/jp/wasabeef/sample/MainActivity.java
+++ sample/src/main/java/jp/wasabeef/sample/MainActivity.java
@@ -15,221 +15,250 @@ import static com.tencent.smtt.sdk.QbSdk.initX5Environment;
 
 public class MainActivity extends AppCompatActivity {
 
-  private RichEditor mEditor;
-  private TextView mPreview;
-  private TextView core;
-  private WebView sysWebview;
+    private RichEditor mEditor;
+    private TextView mPreview;
+    private TextView core;
+    private WebView sysWebview;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 //    initX5Environment(this,null);
-    mEditor = (RichEditor) findViewById(R.id.editor);
-    core = (TextView) findViewById(R.id.core);
-    sysWebview = (WebView) findViewById(R.id.webview);
-    sysWebview.getSettings().setJavaScriptEnabled(true);
-    sysWebview.setWebViewClient(new WebViewClient(){
-      @Override
-      public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        view.loadUrl(url);//根据传入的参数在去加载新的网页
-        return true;//表示当前WebView可以处理打开新网页的请求，不用借助系统浏览器
-      }
+        mEditor = (RichEditor) findViewById(R.id.editor);
+        core = (TextView) findViewById(R.id.core);
+        sysWebview = (WebView) findViewById(R.id.webview);
+        sysWebview.getSettings().setJavaScriptEnabled(true);
+        sysWebview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);//根据传入的参数在去加载新的网页
+                return true;//表示当前WebView可以处理打开新网页的请求，不用借助系统浏览器
+            }
 
-    });
-    sysWebview.loadUrl("http://www.baidu.com");
+        });
+        sysWebview.loadUrl("http://www.baidu.com");
 
-    if(mEditor.getX5WebViewExtension()!=null){
+        if (mEditor.getX5WebViewExtension() != null) {
 //      Toast.makeText(this,"x5 core",Toast.LENGTH_SHORT).show();
-      core.setText("x5 core");
-    }else{
+            core.setText("x5 core");
+        } else {
 //      Toast.makeText(this,"sys core",Toast.LENGTH_SHORT).show();
-      core.setText("sys core");
+            core.setText("sys core");
+        }
+
+        mEditor.setEditorHeight(200);
+        mEditor.setEditorFontSize(22);
+        mEditor.setEditorFontColor(Color.RED);
+        //mEditor.setEditorBackgroundColor(Color.BLUE);
+        //mEditor.setBackgroundColor(Color.BLUE);
+        //mEditor.setBackgroundResource(R.drawable.bg);
+        mEditor.setPadding(10, 10, 10, 10);
+        //mEditor.setBackground("https://raw.githubusercontent.com/wasabeef/art/master/chip.jpg");
+        mEditor.setPlaceholder("Insert text here...");
+        //mEditor.setInputEnabled(false);
+
+        mPreview = (TextView) findViewById(R.id.preview);
+        mEditor.setOnTextChangeListener(new RichEditor.OnTextChangeListener() {
+            @Override
+            public void onTextChange(String text) {
+                mPreview.setText(text);
+            }
+        });
+
+        findViewById(R.id.action_undo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.undo();
+            }
+        });
+
+        findViewById(R.id.action_redo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.redo();
+            }
+        });
+
+        findViewById(R.id.action_bold).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setBold();
+            }
+        });
+
+        findViewById(R.id.action_italic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setItalic();
+            }
+        });
+
+        findViewById(R.id.action_subscript).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setSubscript();
+            }
+        });
+
+        findViewById(R.id.action_superscript).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setSuperscript();
+            }
+        });
+
+        findViewById(R.id.action_strikethrough).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setStrikeThrough();
+            }
+        });
+
+        findViewById(R.id.action_underline).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setUnderline();
+            }
+        });
+
+        findViewById(R.id.action_heading1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setHeading(1);
+            }
+        });
+
+        findViewById(R.id.action_heading2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setHeading(2);
+            }
+        });
+
+        findViewById(R.id.action_heading3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setHeading(3);
+            }
+        });
+
+        findViewById(R.id.action_heading4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setHeading(4);
+            }
+        });
+
+        findViewById(R.id.action_heading5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setHeading(5);
+            }
+        });
+
+        findViewById(R.id.action_heading6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setHeading(6);
+            }
+        });
+
+        findViewById(R.id.action_txt_color).setOnClickListener(new View.OnClickListener() {
+            private boolean isChanged;
+
+            @Override
+            public void onClick(View v) {
+                mEditor.setTextColor(isChanged ? Color.BLACK : Color.RED);
+                isChanged = !isChanged;
+            }
+        });
+
+        findViewById(R.id.action_bg_color).setOnClickListener(new View.OnClickListener() {
+            private boolean isChanged;
+
+            @Override
+            public void onClick(View v) {
+                mEditor.setTextBackgroundColor(isChanged ? Color.TRANSPARENT : Color.YELLOW);
+                isChanged = !isChanged;
+            }
+        });
+
+        findViewById(R.id.action_indent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setIndent();
+            }
+        });
+
+        findViewById(R.id.action_outdent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setOutdent();
+            }
+        });
+
+        findViewById(R.id.action_align_left).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setAlignLeft();
+            }
+        });
+
+        findViewById(R.id.action_align_center).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setAlignCenter();
+            }
+        });
+
+        findViewById(R.id.action_align_right).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setAlignRight();
+            }
+        });
+
+        findViewById(R.id.action_blockquote).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setBlockquote();
+            }
+        });
+
+        findViewById(R.id.action_insert_bullets).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setBullets();
+            }
+        });
+
+        findViewById(R.id.action_insert_numbers).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.setNumbers();
+            }
+        });
+
+        findViewById(R.id.action_insert_image).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.insertImage("http://www.1honeywan.com/dachshund/image/7.21/7.21_3_thumb.JPG",
+                        "dachshund");
+            }
+        });
+
+        findViewById(R.id.action_insert_link).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.insertLink("https://github.com/wasabeef", "wasabeef");
+            }
+        });
+        findViewById(R.id.action_insert_checkbox).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditor.insertTodo();
+            }
+        });
     }
-
-    mEditor.setEditorHeight(200);
-    mEditor.setEditorFontSize(22);
-    mEditor.setEditorFontColor(Color.RED);
-    //mEditor.setEditorBackgroundColor(Color.BLUE);
-    //mEditor.setBackgroundColor(Color.BLUE);
-    //mEditor.setBackgroundResource(R.drawable.bg);
-    mEditor.setPadding(10, 10, 10, 10);
-    //mEditor.setBackground("https://raw.githubusercontent.com/wasabeef/art/master/chip.jpg");
-    mEditor.setPlaceholder("Insert text here...");
-    //mEditor.setInputEnabled(false);
-
-    mPreview = (TextView) findViewById(R.id.preview);
-    mEditor.setOnTextChangeListener(new RichEditor.OnTextChangeListener() {
-      @Override public void onTextChange(String text) {
-        mPreview.setText(text);
-      }
-    });
-
-    findViewById(R.id.action_undo).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.undo();
-      }
-    });
-
-    findViewById(R.id.action_redo).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.redo();
-      }
-    });
-
-    findViewById(R.id.action_bold).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setBold();
-      }
-    });
-
-    findViewById(R.id.action_italic).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setItalic();
-      }
-    });
-
-    findViewById(R.id.action_subscript).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setSubscript();
-      }
-    });
-
-    findViewById(R.id.action_superscript).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setSuperscript();
-      }
-    });
-
-    findViewById(R.id.action_strikethrough).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setStrikeThrough();
-      }
-    });
-
-    findViewById(R.id.action_underline).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setUnderline();
-      }
-    });
-
-    findViewById(R.id.action_heading1).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setHeading(1);
-      }
-    });
-
-    findViewById(R.id.action_heading2).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setHeading(2);
-      }
-    });
-
-    findViewById(R.id.action_heading3).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setHeading(3);
-      }
-    });
-
-    findViewById(R.id.action_heading4).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setHeading(4);
-      }
-    });
-
-    findViewById(R.id.action_heading5).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setHeading(5);
-      }
-    });
-
-    findViewById(R.id.action_heading6).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setHeading(6);
-      }
-    });
-
-    findViewById(R.id.action_txt_color).setOnClickListener(new View.OnClickListener() {
-      private boolean isChanged;
-
-      @Override public void onClick(View v) {
-        mEditor.setTextColor(isChanged ? Color.BLACK : Color.RED);
-        isChanged = !isChanged;
-      }
-    });
-
-    findViewById(R.id.action_bg_color).setOnClickListener(new View.OnClickListener() {
-      private boolean isChanged;
-
-      @Override public void onClick(View v) {
-        mEditor.setTextBackgroundColor(isChanged ? Color.TRANSPARENT : Color.YELLOW);
-        isChanged = !isChanged;
-      }
-    });
-
-    findViewById(R.id.action_indent).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setIndent();
-      }
-    });
-
-    findViewById(R.id.action_outdent).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setOutdent();
-      }
-    });
-
-    findViewById(R.id.action_align_left).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setAlignLeft();
-      }
-    });
-
-    findViewById(R.id.action_align_center).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setAlignCenter();
-      }
-    });
-
-    findViewById(R.id.action_align_right).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setAlignRight();
-      }
-    });
-
-    findViewById(R.id.action_blockquote).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setBlockquote();
-      }
-    });
-
-    findViewById(R.id.action_insert_bullets).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setBullets();
-      }
-    });
-
-    findViewById(R.id.action_insert_numbers).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.setNumbers();
-      }
-    });
-
-    findViewById(R.id.action_insert_image).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.insertImage("http://www.1honeywan.com/dachshund/image/7.21/7.21_3_thumb.JPG",
-            "dachshund");
-      }
-    });
-
-    findViewById(R.id.action_insert_link).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.insertLink("https://github.com/wasabeef", "wasabeef");
-      }
-    });
-    findViewById(R.id.action_insert_checkbox).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mEditor.insertTodo();
-      }
-    });
-  }
 }
